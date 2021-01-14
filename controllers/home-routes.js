276a144407e5dad6fd998bfd3dc0b893fdf32dac
Joshua Sung <yuniksung@gmail.com>
2021-01-13 20:58:57 -0600

    login and logout function has been updated

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,6 +4,7 @@ const { Post, User, Comment, Vote } = require('../models');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
+  console.log(req.session);
   console.log('======================');
   Post.findAll({
     attributes: [
@@ -40,6 +41,11 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
   res.render('login');
 });
 
