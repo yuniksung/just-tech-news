a443685fafc58fcd46cea32de32bfbb9cd58145a
Joshua Sung <yuniksung@gmail.com>
2021-01-15 21:10:42 -0600

    confirming

--- utils/helpers.js
+++ utils/helpers.js
@@ -5,18 +5,19 @@ module.exports = {
       ).getFullYear()}`;
     },
     format_url: url => {
-        return url
-          .replace('http://', '')
-          .replace('https://', '')
-          .replace('www.', '')
-          .split('/')[0]
-          .split('?')[0];
-      },
+      return url
+        .replace('http://', '')
+        .replace('https://', '')
+        .replace('www.', '')
+        .split('/')[0]
+        .split('?')[0];
+    },
     format_plural: (word, amount) => {
-        if (amount !== 1) {
-          return `${word}s`;
-        }
-    
-        return word;
+      if (amount !== 1) {
+        return `${word}s`;
       }
+  
+      return word;
+    }
   };
+  
