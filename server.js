0e23382af9a433bf376ff8653ca9de17130b7014
Joshua Sung <yuniksung@gmail.com>
2021-01-15 21:05:27 -0600

    testing for format date/ plural has passed

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
 
 
 const session = require('express-session');
